99e2313df0743ff3bc62863dcd0734c4fb349e83
moein fadakar <moeinfadakar8@gmail.com>
2024-08-24 22:30:52 +0330

    Add files via upload

---
+++ python-se5/python/chess_board.py
@@ -0,0 +1,9 @@
+from functions import functions
+
+row = int(input("Enter The row pls:"))
+col = int(input("Enter The col pls:"))
+
+functions.chess_border_maker(row,col)
+functions.bye()
+
+
---
+++ python-se5/python/pascal_triangle.py
@@ -0,0 +1,7 @@
+from functions import  functions
+
+user_number = int(input("Enter The Number:"))
+
+functions.generate_pascal_triangle(user_number)
+functions.bye()
+
